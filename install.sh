4dffab85fb9a6f38bc71b7833c471939416cf008
Daragh Hollman <hollmandaragh@gmail.com>
2024-08-23 14:58:33 +0100

    Fixed removing yay directory

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -Syu wayland waybar hyprland foot ranger aerc newsboat mako wofi zathura zathura-pdf-poppler neovim cronie tree-sitter npm dotnet-sdk unzip ttf-nerd-fonts-symbols
 
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -r yay/
+sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si && cd .. && rm -rf yay/
 
 sudo yay -S ttf-courier-prime
 
