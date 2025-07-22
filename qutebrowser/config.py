e2125f89a628ae9e9f98b4de02a5b278c671d4f8
Daragh Hollman <hollmandaragh@gmail.com>
2025-07-22 19:05:09 +0100

    Add gruvbox style to qutebrowser

---
+++ qutebrowser/config.py
@@ -0,0 +1,11 @@
+config.load_autoconfig()
+
+c.tabs.background = True
+c.new_instance_open_target = "window"
+c.downloads.position = "bottom"
+
+# c.fonts.tabs = '8pt monospace'
+# c.fonts.statusbar = '8pt monospace'
+# c.fonts.web.family.fantasy = "Arial"
+
+config.source("gruvbox.py")
