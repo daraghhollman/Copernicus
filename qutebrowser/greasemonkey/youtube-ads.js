24cefe4c8bcc19eec0183c7c441b3f9e0b6fed8a
Daragh Hollman <hollmandaragh@gmail.com>
2025-10-01 12:23:57 +0100

    js to skip ads

--- qutebrowser/greasemonkey/youtube-ads.js
+++ qutebrowser/greasemonkey/youtube-ads.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name Skip YouTube ads
-// @description Skips the ads in YouTube videos
-// @run-at document-start
-// @include *.youtube.com/*
+// @name         Auto Skip YouTube Ads
+// @version      1.0.0
+// @description  Speed up and skip YouTube ads automatically
+// @author       jso8910
+// @match        *://*.youtube.com/*
+// @exclude      *://*.youtube.com/subscribe_embed?*
 // ==/UserScript==
-
-document.addEventListener('load', () => {
-	const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button-modern')
+setInterval(() => {
+	const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button')
 	if (btn) {
 		btn.click()
 	}
 	const ad = [...document.querySelectorAll('.ad-showing')][0];
 	if (ad) {
-		document.querySelector('video').currentTime = 9999999999;
+		// document.querySelector('video').playbackRate = 10;
+		const vid = document.querySelector('video');
+		vid.currentTime = vid.duration;
 	}
-}, true);
-
+}, 50)
