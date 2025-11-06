3acb445550eb4189d173dd08df204fd584e8f633
Daragh Hollman <hollmandaragh@gmail.com>
2025-11-06 21:42:46 +0000

    Update adblock script

--- qutebrowser/greasemonkey/youtube-ads.js
+++ qutebrowser/greasemonkey/youtube-ads.js
@@ -7,14 +7,14 @@
 // @exclude      *://*.youtube.com/subscribe_embed?*
 // ==/UserScript==
 setInterval(() => {
-	const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button')
-	if (btn) {
-		btn.click()
-	}
-	const ad = [...document.querySelectorAll('.ad-showing')][0];
-	if (ad) {
-		// document.querySelector('video').playbackRate = 10;
-		const vid = document.querySelector('video');
-		vid.currentTime = vid.duration;
-	}
+    const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button')
+    if (btn) {
+        btn.click()
+    }
+    const ad = [...document.querySelectorAll('.ad-showing')][0];
+    if (ad) {
+        document.querySelector('video').playbackRate = 10;
+        // const vid = document.querySelector('video');
+        // vid.currentTime = vid.duration - 1;
+    }
 }, 50)
